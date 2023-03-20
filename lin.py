d285ea764f8abb2f1a90736c8ed995631b48efb8
ADKill Player Offline <yndoc@users.noreply.github.com>
2023-03-20 10:41:50 +0800

    Update lin.py

--- lin.py
+++ lin.py
@@ -6,22 +6,22 @@ import time
 #qq:1628708538
 
 def main(username,password):
-    chusihua = requests.get('https://bbs.binmt.cc/misc.php?mod=mobile',headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44'})
-    saltkey = re.findall('cQWy_2132_saltkey=(.*?);',str(chusihua.headers))[0]
+    chusihua = requests.get('https://bbs.binmt.cc/misc.php?mod=mobile',headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'})
+    saltkey = re.findall('saltkey=(.*?);',str(chusihua.headers))[0]
     formhash = re.findall('formhash=(.*?)&',str(chusihua.text))[0]
     #print(formhash)
-    data = "formhash=" + formhash + "&referer=https%3A%2F%2Fbbs.binmt.cc%2Fk_misign-sign.html&loginfield=username&username=" + username + "&password=" + password + "&questionid=0&answer=&submit=true";
-    headers = {'Cookie': 'cQWy_2132_saltkey=' + saltkey,'Content-type': 'application/x-www-form-urlencoded','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44'}
+    data = "formhash=" + formhash + "&referer=https%3A%2F%2Fbbs.binmt.cc%2Fk_misign-sign.html&fastloginfield=username&cookietime=31104000&username=" + username + "&password=" + password + "&questionid=0&answer=&submit=true";
+    headers = {'Cookie': 'cQWy_2132_saltkey=' + saltkey,'Content-type': 'text/html; charset=utf-8','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
     #print(headers)
     denlu = requests.post('https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=&handlekey=loginform&inajax=1',data=data.encode("utf-8"),headers=headers)
     #print(denlu.text)
     cQWy_2132_auth = re.findall('cQWy_2132_auth=(.*?);',str(denlu.headers))[0]
     #print(cQWy_2132_auth)
-    headers = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44","Cookie": "cQWy_2132_saltkey=" + saltkey + ";cQWy_2132_auth=" + cQWy_2132_auth}
+    headers = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36","Cookie": "cQWy_2132_saltkey=" + saltkey + ";cQWy_2132_auth=" + cQWy_2132_auth}
     huo = requests.get('https://bbs.binmt.cc/k_misign-sign.html',headers=headers)
     formhash = re.findall('formhash=(.*?)&amp',str(huo.text))[0]
     data = {"operation":"qiandao","formhash": formhash,"Cookie":"cQWy_2132_saltkey=" + saltkey + ";cQWy_2132_auth=" + cQWy_2132_auth}
-    headers = {"Cookie":"cQWy_2132_saltkey=" + saltkey + ";cQWy_2132_auth=" + cQWy_2132_auth,"Referer":"https://bbs.binmt.cc/forum.php","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44"}
+    headers = {"Cookie":"cQWy_2132_saltkey=" + saltkey + ";cQWy_2132_auth=" + cQWy_2132_auth,"Referer":"https://bbs.binmt.cc/forum.php","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
     qd1 = requests.post('https://bbs.binmt.cc/k_misign-sign.html',data=data,headers=headers)
     qd = requests.get('https://bbs.binmt.cc/k_misign-sign.html',data=data,headers=headers)
     #print(qd.text)
@@ -32,6 +32,7 @@ def main(username,password):
         print(time.strftime("%H时%M分%S秒", time.localtime(time.time()+28800)))
     else:
         print('签到失败')
+
 
 if __name__ == '__main__':
     #账号
